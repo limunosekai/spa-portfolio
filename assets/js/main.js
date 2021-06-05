42de9b8f47b6ee458c0e4e45708de8e48cf417ed
limunosekai <powerlsh0103@gmail.com>
2021-06-05 21:37:39 +0900

    about 페이지 수정 & 로딩 화면 추가 & 프로그래스바 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
+const loading = document.querySelector('.loading');
+const bar = document.querySelector('.progressBar');
+
+// 스크롤 top 좌표
+let scrollTop = 0;
+
+// 로딩
+const loadingStart = () => {
+  loading.classList.add('active');
+};
+
+loadingStart();
+
+// progress bar
+const progressHandler = (e) => {
+  scrollTop = document.documentElement.scrollTop;
+
+  let per = Math.ceil(
+    (scrollTop / (document.body.scrollHeight - window.innerHeight)) * 100
+  );
+  bar.style.height = per + '%';
+};
+
 window.onload = () => {
+  loading.classList.remove('active'); // 로딩 종료
+
   window.addEventListener('scroll', (e) => {
     if (window.pageYOffset > 100) {
       document.querySelector('header').classList.add('is-scrolling');
@@ -7,3 +32,5 @@ window.onload = () => {
     }
   });
 };
+
+window.addEventListener('scroll', progressHandler, false);
